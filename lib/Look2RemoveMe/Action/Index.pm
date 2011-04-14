163a60aac70e9cebf494f79402da5ef243f77bf9
vti <viacheslav.t@gmail.com>
2011-04-14 13:24:03 +0300

    Added comments

--- lib/Look2RemoveMe/Action/Index.pm
+++ lib/Look2RemoveMe/Action/Index.pm
@@ -6,6 +6,10 @@ use warnings;
 use base 'Look2RemoveMe::Action';
 
 sub run {
+    my $self = shift;
+
+    my $comments = $self->req->param('comments');
+    $self->set_var(comments => defined $comments ? 1 : 0);
 }
 
 1;
